9f89c0028519cb27b42d28de5107dad60f5b8470
skanukov <s.kanukov@gmail.com>
2017-12-21 17:21:32 +0300

    Refactor injector

--- injector.go
+++ injector.go
@@ -6,7 +6,7 @@ func Inject(result interface{}, dependencies ...interface{}) error {
 	objects := []*inject.Object{
 		{Value: result},
 	}
-	appendDependencyObjects(&objects, dependencies)
+	objects = appendDependencyObjects(objects, dependencies)
 
 	var graph inject.Graph
 	if err := graph.Provide(objects...); err != nil {
@@ -16,12 +16,13 @@ func Inject(result interface{}, dependencies ...interface{}) error {
 }
 
 func appendDependencyObjects(
-	objects *[]*inject.Object, dependencies []interface{},
-) {
+	objects []*inject.Object, dependencies []interface{},
+) []*inject.Object {
 	for _, dep := range dependencies {
-		*objects = append(*objects, &inject.Object{
+		objects = append(objects, &inject.Object{
 			Value:    dep,
 			Complete: true,
 		})
 	}
+	return objects
 }
